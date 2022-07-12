7317f1ddf3bf071665d5a0982db0f56d97db2610
Ross <rossgarfield@gmail.com>
2022-07-12 11:25:34 -0500

    all routes done

--- app/controllers/bikerides_controller.rb
+++ app/controllers/bikerides_controller.rb
@@ -1,2 +1,28 @@
 class BikeridesController < ApplicationController
+    before_action :find_bikeride, only: :show
+
+    def index
+        bikerides = Bikeride.all
+        render json: bikerides
+    end
+
+    def show
+        render json: @bikeride
+    end
+
+    def create
+        bikeride = Bikeride.create!(bikeride_params)
+        render json: bikeride, status: :created
+    end
+
+
+    private
+
+    def bikeride_params
+        params.permit(:bike_id, :start, :end)
+    end
+
+    def find_bikeride
+        @bikeride = Bikeride.find(params[:id])
+    end
 end
--- app/serializers/bikeride_serializer.rb
+++ app/serializers/bikeride_serializer.rb
@@ -2,5 +2,4 @@ class BikerideSerializer < ActiveModel::Serializer
   attributes :id, :start, :end
   has_one :renter
   has_one :owner
-  has_one :bike
 end
